02edf1875c4cbf5b6c303846683977c9ca4cfc2c
Cameron King <cameron.king@ecc.email>
2015-07-17 03:57:35 +0000

    lesson list pulls from api

--- assets/js/500_controllers.js
+++ assets/js/500_controllers.js
@@ -42,48 +42,16 @@ bsmWebAppControllers.controller('TestCtrl', [
 ]);
 
 bsmWebAppControllers.controller('LessonListCtrl', [
-    '$scope', 'Page',
-    function ($scope, Page) {
+    '$scope', '$http', 'Page',
+    function ($scope, $http, Page) {
         Page.setStyle("frontpage");
         Page.setTitle("Lesson List");
-        $scope.lessons = [
-            {
-                'id': 1,
-                'name': 'God Made the World'
-            },
-            {
-                'id': 2,
-                'name': 'Jesus Loves the Little Children'
-            },
-            {
-                'id': 3,
-                'name': 'Sample Title 3'
-            },
-            {
-                'id': 4,
-                'name': 'Sample Title 4'
-            },
-            {
-                'id': 5,
-                'name': 'Sample Title 5'
-            },
-            {
-                'id': 6,
-                'name': 'Sample Title 6'
-            },
-            {
-                'id': 7,
-                'name': 'Sample Title 7'
-            },
-            {
-                'id': 8,
-                'name': 'Sample Title 8'
-            },
-            {
-                'id': 9,
-                'name': 'Sample Title 9'
-            }
-        ];
+        $http.get('/api/v1/lesson').
+            success(function(data, status, headers, config) {
+                $scope.lessons = data;
+            }).error(function(data, status, headers, config) {
+                // handle error
+            });
     }
 ]);
 
