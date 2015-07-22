352982def4bf9faae4834188d78dc8180068d912
Cameron King <cameron.king@ecc.email>
2015-07-22 22:56:59 +0000

    remove sample lesson return

--- src/bsm/api/lesson/retrieve.go
+++ src/bsm/api/lesson/retrieve.go
@@ -68,30 +68,4 @@ func Retrieve(w http.ResponseWriter, r *http.Request) (interface{}, error) {
         Name: lesson.Title,
         Deck: slides,
     }, nil
-    
-    return Lesson {
-        Id: int(iId),
-        Name: "Example Lesson",
-        Deck: slide.Slides {
-            slide.NewTitleSlide(
-                "Title Slide", 
-                "Title Slide", 
-                "A slide with a title",
-            ),
-            slide.NewImageSlide(
-                "Image Slide",
-                "Image Slide",
-                "https://placehold.it/300?text=image",
-                "An image caption",
-            ),
-            slide.NewNarrationSlide(
-                "Narration Slide",
-                character.NewCharacter(
-                    "Narrarator",
-                    "https://placehold.it/64?text=narr",
-                ),
-                "Lorem ipsum dolor sit amet.",
-            ),
-        },
-    }, nil
 }
