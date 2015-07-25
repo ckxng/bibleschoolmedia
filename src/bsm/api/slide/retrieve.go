bea23978de4cc85edfc009d42e4d636e34c90ce2
Cameron King <cameron.king@ecc.email>
2015-07-25 01:49:45 +0000

    unimpliment api/slide/retrieve, not needed

--- src/bsm/api/slide/retrieve.go
+++ src/bsm/api/slide/retrieve.go
@@ -1,54 +1,11 @@
 package slide
 
 import (
-    "github.com/gorilla/mux"
     "fmt"
     "net/http"
-    "strconv"
-    "bsm/api/character"
 )
 
+// returns an error.  There are no current plans to impliment this function.
 func Retrieve(w http.ResponseWriter, r *http.Request) (interface{}, error) {
-    id, ok := mux.Vars(r)["id"]
-    if !ok || id == "" {
-        return nil, fmt.Errorf("Missing ID")
-    }
-    
-    iId, err := strconv.ParseInt(id,0,0)
-    if err != nil {
-        return nil, fmt.Errorf("Non-numeric ID: %s", id)
-    }
-    
-    switch int64(iId) {
-        case 0:
-            ts := NewTitleSlide(
-                "Title Slide", 
-                "Title Slide", 
-                "A slide with a title",
-            )
-            ts.id = int64(iId)
-            return ts, nil
-        case 1:
-            is := NewImageSlide(
-                "Image Slide",
-                "Image Slide",
-                "https://placehold.it/300?text=image",
-                "An image caption",
-            )
-            is.id = int64(iId)
-            return is, nil
-        case 2: 
-            ns := NewNarrationSlide(
-                "Narration Slide",
-                character.NewCharacter(
-                    "Narrarator",
-                    "https://placehold.it/64?text=narr",
-                ),
-                "Lorem ipsum dolor sit amet.",
-            )
-            ns.id = int64(iId)
-            return ns, nil
-    }
-    
-    return nil, fmt.Errorf("Invalid slide")
+    return nil, fmt.Errorf("Not implimented")
 }
